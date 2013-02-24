c9b579883cb8d0eb44579563c9ebc43a712fc7f3
Adrian Quevedo <hello@adrianquevedo.com>
2013-02-23 20:46:24 -0500

    Add events
    Include Google Analytics Event Tracking

--- js/actions.js
+++ js/actions.js
@@ -1,9 +1,16 @@
 var NS = NS || {};
 NS.Site = (function(){
+	'use strict';
 	$(document).on('ready', function(){
 		$('#id').on('click',function(e) {
 			//actions
 			e.preventDefault();
 		});
+
+		//GOOGLE ANALYTICS EVENTS
+        $('.btn').on('click', function(){
+            var title = $(this).attr('title');
+            _gaq.push(['_trackEvent', 'Button: ' + title, 'Click', $(this).attr('href')]);
+        });
 	});
 }());
